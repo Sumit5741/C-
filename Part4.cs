a8161db6899cbbde423eec3c973eef69fe74b6c1
Sumit <33118054+Sumit5741@users.noreply.github.com>
2021-12-02 22:04:55 +0530

    Add files via upload

---
+++ Part4.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace SamplePart4
+{
+    class Part4
+    {
+        public static void Main(String[] args)
+        {
+            Console.WriteLine(@"\\Hello");   // Verbatim Literal
+            String name = "C\\OuterFile\\InnerFile";
+            Console.WriteLine("Old File Location is : {0}", name);
+            name = @"C\OuterFile\InnerFile1";
+            Console.WriteLine("New File Location is : {0}", name);
+            name = "\"Sumit\"";
+            Console.WriteLine(name);
+        }
+
+    }
+}
