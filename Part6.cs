5790833fac488ea423dad82e146a12c5e28a1de6
Sumit <33118054+Sumit5741@users.noreply.github.com>
2021-12-02 22:04:35 +0530

    Add files via upload

---
+++ Part6.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace PartSixNullableTypes
+{
+    class Part6
+    {
+        public static void Main(String [] args)
+        {
+            String CharacterIsNUllable = null;
+            int? TicketsOnSale = null;
+            int AvailableTickets;
+            Console.WriteLine("Value Type can not be nullable");
+            Console.WriteLine("Reference Type can be nullable"); 
+            if(TicketsOnSale == null)
+            {
+                Console.WriteLine("TicketsOnSale is null");
+            }
+            else
+            {
+                Console.WriteLine("TicketsOnSale is not null");
+            }
+            AvailableTickets = TicketsOnSale ?? 0;
+            Console.WriteLine("AvailableTickets is : {0}", AvailableTickets);
+        }
+    }
+}
---
+++ Part7.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace TypeConversions
+{
+    class Part7
+    {
+        public static void Main(String[] args)
+        {
+            Console.WriteLine("Type Conversion");
+            int a = 10;
+            float f = a;
+            Console.WriteLine(a);
+            Console.WriteLine("After Implicit Conversion the value is f is : {0}",f);
+            float f1 = 12.9999f;
+            int i1 = (int)f1;
+            int i2 = Convert.ToInt16(f1);
+            Console.WriteLine(f1);
+            Console.WriteLine("After Explicit conversion the value of i1 is : {0}", i1);
+            Console.WriteLine("After Explicit conversion the value of i2 is : {0}", i2);
+        }
+    }
+}
